59b58643ed2d7d19386485879b07825424db4c21
Jarrod <jsgriffin96@live.com>
2020-09-13 15:36:15 -0700

    added vectors, matrix, dataframes with examples of each

--- R Workshop 2 Refrence.R
+++ R Workshop 2 Refrence.R
@@ -15,7 +15,7 @@ typeof(intExample)
 
 ####Converting between data types
 
-##Good
+##Good ideas
 
 #Character to numeric
 price <- "1450"
@@ -32,4 +32,72 @@ zipcode <- zipcode + 15 #this makes no sense, why would we do this?
 zipcode <- as.character(zipcode) #storing as char prevents the above
 
 
+##Bad Ideas
+
+#decimal numeric to int
+taxRate <- 0.03 # 3% tax rate
+taxRate <- as.integer(taxRate) #turns to 0
+
+
+#decimal numeric to character
+gravity <- 9.81
+gravity <- as.character(gravity)
+
+
+
+
+####Vectors
+sampleVector <- c(1,2,3)
+
+#Named Vector
+temperatures <- c(75,76,80,77,73,71,69)
+names(temperatures) <- c('Mon','Tues','Wed','Thurs','Fri','Sat','Sun')
+temperatures
+
+#Select data
+over.74 <- temperatures>74
+over.74
+temperatures[over.74]
+temps.over.74 <- temperatures[over.74]
+
+#Combine vectors
+a <- c(1,2,3)
+b <- c(4,5,6)
+ab <- c(a,b)
+
+
+####Matrices
+sampleMatrix <- matrix(data=c(1,2,3,4), nrow = 2, ncol = 2, byrow = FALSE) #change byrow 
+sampleMatrix
+
+#add row to matrix
+sampleMatrix <- rbind(sampleMatrix,c(5,6))
+sampleMatrix
+
+#add column to matrix
+sampleMatrix <- cbind(sampleMatrix,c(7,8,9))
+sampleMatrix
+
+
+
+#### Data Frames
+name <- c('Jarrod','Jillian','Patrick','Grant')
+age <- c(24,21,24,19)
+iq <- c(210,480,200,5)
+standing <- c('Super Senior','Senior','Super Senior','Junior')
+
+eboard.df <- data.frame(name,age,iq,standing)
+
+is.data.frame(eboard.df)
+
+#Lets change Grant's IQ
+eboard.df[4,3] <- 500
+
+#accessing columns
+eboard.df$name
+eboard.df$iq
+
+#An example of functions 
+mean(eboard.df$age)
+mean(eboard.df$iq)
 
